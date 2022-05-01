c6c033fde9c215466fd23772594fe120489446fc
Victor N. Skurikhin <vs@lnhost.net>
2022-05-01 21:29:29 +0300

    Read XML with DOM parser #13

--- src/main/java/su/svn/readXML/services/DOMParserServiceImpl.java
+++ src/main/java/su/svn/readXML/services/DOMParserServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * This file was last modified at 2022.05.01 19:24 by Victor N. Skurikhin.
+ * This file was last modified at 2022.05.01 21:29 by Victor N. Skurikhin.
  * This is free and unencumbered software released into the public domain.
  * For more information, please refer to <http://unlicense.org>
  * DOMParserServiceImpl.java
@@ -8,8 +8,9 @@
 
 package su.svn.readXML.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -25,9 +26,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
+import static su.svn.readXML.Main.SPACES;
+
+@Slf4j
 @Service
 public class DOMParserServiceImpl {
+    public static final String prefixSymbol = "|   ";
+
     public void parseData(String xmlData, String xmlSchemaFileName) {
         parse(new ByteArrayInputStream(xmlData.getBytes()), xmlSchemaFileName);
     }
@@ -51,8 +59,53 @@ public class DOMParserServiceImpl {
             schema = schemaFactory.newSchema(new File(schemaFileName));
             Validator validator = schema.newValidator();
             validator.validate(new DOMSource(document));
+
+            //Normalize the XML Structure; It's just too important !!
+            document.getDocumentElement().normalize();
+
+            //Here comes the root node
+            Element root = document.getDocumentElement();
+            log.info("XML root: [[{}]]", root.getNodeName());
+
+            NodeList nList = document.getElementsByTagName("employee");
+
+            visitChildNodes(nList, 1);
         } catch (ParserConfigurationException | IOException | SAXException e) {
             throw new RuntimeException(e);  // TODO custom Exception
+        }
+    }
+
+    //This function is called recursively
+    private static void visitChildNodes(NodeList nList, int level) {
+
+        final String prefixSeparator = prefixSymbol.repeat(level);
+
+        for (int j = 0; j < nList.getLength(); j++) {
+            Node node = nList.item(j);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                String content = Arrays.stream(node.getTextContent().split(SPACES))
+                        .filter(s -> s != null && s.length() > 0)
+                        .collect(Collectors.joining(" || "));
+                log.info("{}Node name: [[{}]] Value: [[{}]]", prefixSeparator, node.getNodeName(), content);
+                //Check all attributes
+                if (node.hasAttributes()) {
+                    // get attributes names and values
+                    NamedNodeMap nodeMap = node.getAttributes();
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Node tempNode = nodeMap.item(i);
+                        log.info(
+                                "{}Attr name: [[{}]] Value: [[{}]]",
+                                prefixSeparator,
+                                tempNode.getNodeName(),
+                                tempNode.getNodeValue()
+                        );
+                    }
+                }
+                if (node.hasChildNodes()) {
+                    //We got more childs; Let's visit them as well
+                    visitChildNodes(node.getChildNodes(), level + 1);
+                }
+            }
         }
     }
 }
